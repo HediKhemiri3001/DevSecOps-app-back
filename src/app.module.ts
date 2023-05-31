193e15f2601e233db4002878fe4902fdab09c08b
KHEMIRI Mohamed Hedi <khemirimedhedi@gmail.com>
2023-05-31 20:00:11 +0100

    Fixed database type

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        type: 'postgres' as 'postgres',
+        type: 'mysql' as 'mysql',
         host: configService.get<string>('DATABASE_HOST'),
         port: parseInt(configService.get<string>('DATABASE_PORT')),
         username: configService.get<string>('DATABASE_USER'),
